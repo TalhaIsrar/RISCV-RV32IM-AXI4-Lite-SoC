0e6ef6c884c0e75f682e06abeb953ced3f094c3d
TalhaIsrar <talhaisrar02@gmail.com>
2025-10-30 12:16:49 +0000

    Replaces AXi4 lite Master and slave with interface to clean code

--- src/axi4_lite/axi4_lite_master.sv
+++ src/axi4_lite/axi4_lite_master.sv
@@ -19,33 +19,7 @@ module axi4_lite_master #(
     output logic                   read_busy,
 
     // Unified AXI4-Lite interface
-
-    // WRIE ADDRESS CHANNEL
-    output logic [ADDR_WIDTH - 1 : 0]   M_AXI_AWADDR,   // Write address
-    output logic                        M_AXI_AWVALID,  // Write address valid
-    input logic                         M_AXI_AWREADY,  // Slave ready to accept address
-
-    // WRITE DATA CHANNEL
-    output logic [DATA_WIDTH - 1 : 0]   M_AXI_WDATA,    // Write data
-    output logic [3:0]                  M_AXI_WSTRB,    // Write data byte enable
-    output logic                        M_AXI_WVALID,   // Write data is valid
-    input logic                         M_AXI_WREADY,   // Slave ready to accept data
-
-    // WRITE RESPONSE CHANNEL
-    input logic [1:0]                   M_AXI_BRESP,    // Slave response - Unused here
-    input logic                         M_AXI_BVALID,   // Write response valid from slave
-    output logic                        M_AXI_BREADY,   // Master ready to accept response
-
-    // READ ADDRESS CHANNEL
-    output logic [ADDR_WIDTH - 1 : 0]   M_AXI_ARADDR,   // Read address
-    output logic                        M_AXI_ARVALID,  // Read address valid
-    input logic                         M_AXI_ARREADY,  // Slave ready to accept address
-
-    // READ DATA CHANNEL
-    input logic [DATA_WIDTH - 1 : 0]    M_AXI_RDATA,    // Read data
-    input logic [1:0]                   M_AXI_RRESP,    // Slave response
-    input logic                         M_AXI_RVALID,   // Read data from slave is valid
-    output logic                        M_AXI_RREADY    // Master is ready to accept data
+    axi4_lite_if #(ADDR_WIDTH, DATA_WIDTH) master_if
 );
 
     // ------------------------------
@@ -62,16 +36,16 @@ module axi4_lite_master #(
         .write_strobe(write_strobe),
         .write_busy(write_busy),
 
-        .M_AXI_AWADDR(M_AXI_AWADDR),
-        .M_AXI_AWVALID(M_AXI_AWVALID),
-        .M_AXI_AWREADY(M_AXI_AWREADY),
-        .M_AXI_WDATA(M_AXI_WDATA),
-        .M_AXI_WSTRB(M_AXI_WSTRB),
-        .M_AXI_WVALID(M_AXI_WVALID),
-        .M_AXI_WREADY(M_AXI_WREADY),
-        .M_AXI_BRESP(M_AXI_BRESP),
-        .M_AXI_BVALID(M_AXI_BVALID),
-        .M_AXI_BREADY(M_AXI_BREADY)
+        .M_AXI_AWADDR(master_if.AWADDR),
+        .M_AXI_AWVALID(master_if.AWVALID),
+        .M_AXI_AWREADY(master_if.AWREADY),
+        .M_AXI_WDATA(master_if.WDATA),
+        .M_AXI_WSTRB(master_if.WSTRB),
+        .M_AXI_WVALID(master_if.WVALID),
+        .M_AXI_WREADY(master_if.WREADY),
+        .M_AXI_BRESP(master_if.BRESP),
+        .M_AXI_BVALID(master_if.BVALID),
+        .M_AXI_BREADY(master_if.BREADY)
     );
 
     // ------------------------------
@@ -87,13 +61,13 @@ module axi4_lite_master #(
         .read_data(read_data),
         .read_busy(read_busy),
 
-        .M_AXI_ARADDR(M_AXI_ARADDR),
-        .M_AXI_ARVALID(M_AXI_ARVALID),
-        .M_AXI_ARREADY(M_AXI_ARREADY),
-        .M_AXI_RDATA(M_AXI_RDATA),
-        .M_AXI_RRESP(M_AXI_RRESP),
-        .M_AXI_RVALID(M_AXI_RVALID),
-        .M_AXI_RREADY(M_AXI_RREADY)
+        .M_AXI_ARADDR(master_if.ARADDR),
+        .M_AXI_ARVALID(master_if.ARVALID),
+        .M_AXI_ARREADY(master_if.ARREADY),
+        .M_AXI_RDATA(master_if.RDATA),
+        .M_AXI_RRESP(master_if.RRESP),
+        .M_AXI_RVALID(master_if.RVALID),
+        .M_AXI_RREADY(master_if.RREADY)
     );
 
 endmodule
--- src/axi4_lite/axi4_lite_slave.sv
+++ src/axi4_lite/axi4_lite_slave.sv
@@ -21,33 +21,7 @@ module axi4_lite_slave #(
     output logic [ADDR_WIDTH-1:0]  read_addr,
 
     // Unified AXI4-Lite interface
-
-    // WRIE ADDRESS CHANNEL
-    input logic [ADDR_WIDTH - 1 : 0]    S_AXI_AWADDR,   // Write address
-    input logic                         S_AXI_AWVALID,  // Write address valid
-    output logic                        S_AXI_AWREADY,  // Slave ready to accept address
-
-    // WRITE DATA CHANNEL
-    input logic [DATA_WIDTH - 1 : 0]    S_AXI_WDATA,    // Write data
-    input logic [3:0]                   S_AXI_WSTRB,    // Write data byte enable
-    input logic                         S_AXI_WVALID,   // Write data is valid
-    output logic                        S_AXI_WREADY,   // Slave ready to accept data
-
-    // WRITE RESPONSE CHANNEL
-    output logic [1:0]                  S_AXI_BRESP,    // Slave response - Unused here
-    output logic                        S_AXI_BVALID,   // Write response valid from slave
-    input logic                         S_AXI_BREADY,   // Master ready to accept response
-
-    // READ ADDRESS CHANNEL
-    input logic [ADDR_WIDTH - 1 : 0]    S_AXI_ARADDR,   // Read address
-    input logic                         S_AXI_ARVALID,  // Read address valid
-    output logic                        S_AXI_ARREADY,  // Slave ready to accept address
-
-    // READ DATA CHANNEL
-    output logic [DATA_WIDTH - 1 : 0]   S_AXI_RDATA,    // Read data
-    output logic [1:0]                  S_AXI_RRESP,    // Slave response
-    output logic                        S_AXI_RVALID,   // Read data from slave is valid
-    input logic                         S_AXI_RREADY    // Master is ready to accept data
+    axi4_lite_if #(ADDR_WIDTH, DATA_WIDTH) slave_if
 );
 
     // ----------------------------------
@@ -64,16 +38,16 @@ module axi4_lite_slave #(
         .addr(write_addr),
         .write_data(write_data),
     
-        .S_AXI_AWADDR(S_AXI_AWADDR),
-        .S_AXI_AWVALID(S_AXI_AWVALID),
-        .S_AXI_AWREADY(S_AXI_AWREADY),
-        .S_AXI_WDATA(S_AXI_WDATA),
-        .S_AXI_WSTRB(S_AXI_WSTRB),
-        .S_AXI_WVALID(S_AXI_WVALID),
-        .S_AXI_WREADY(S_AXI_WREADY),
-        .S_AXI_BRESP(S_AXI_BRESP),
-        .S_AXI_BVALID(S_AXI_BVALID),
-        .S_AXI_BREADY(S_AXI_BREADY)
+        .S_AXI_AWADDR(slave_if.AWADDR),
+        .S_AXI_AWVALID(slave_if.AWVALID),
+        .S_AXI_AWREADY(slave_if.AWREADY),
+        .S_AXI_WDATA(slave_if.WDATA),
+        .S_AXI_WSTRB(slave_if.WSTRB),
+        .S_AXI_WVALID(slave_if.WVALID),
+        .S_AXI_WREADY(slave_if.WREADY),
+        .S_AXI_BRESP(slave_if.BRESP),
+        .S_AXI_BVALID(slave_if.BVALID),
+        .S_AXI_BREADY(slave_if.BREADY)
     );
 
     // ----------------------------------
@@ -89,13 +63,13 @@ module axi4_lite_slave #(
         .data_valid(data_valid),
         .addr(read_addr),
 
-        .S_AXI_ARADDR(S_AXI_ARADDR),
-        .S_AXI_ARVALID(S_AXI_ARVALID),
-        .S_AXI_ARREADY(S_AXI_ARREADY),
-        .S_AXI_RDATA(S_AXI_RDATA),
-        .S_AXI_RRESP(S_AXI_RRESP),
-        .S_AXI_RVALID(S_AXI_RVALID),
-        .S_AXI_RREADY(S_AXI_RREADY)
+        .S_AXI_ARADDR(slave_if.ARADDR),
+        .S_AXI_ARVALID(slave_if.ARVALID),
+        .S_AXI_ARREADY(slave_if.ARREADY),
+        .S_AXI_RDATA(slave_if.RDATA),
+        .S_AXI_RRESP(slave_if.RRESP),
+        .S_AXI_RVALID(slave_if.RVALID),
+        .S_AXI_RREADY(slave_if.RREADY)
     );
 
 endmodule
